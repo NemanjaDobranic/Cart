ab4444784d60d610ae361779df49f68879c3635f
Nemanja <dobranicnemanja2014@gmail.com>
2022-09-18 14:23:52 +0200

    changed form styling

--- src/modules/product/form/Form.js
+++ src/modules/product/form/Form.js
@@ -4,10 +4,20 @@ import { connect } from "react-redux";
 import { getPrice } from "../../../resources/commonFunctions/commonFunctions";
 
 class Form extends Component {
+  addToCart = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
+  handleChange = (attribute, item) => {
+    console.log(attribute);
+    console.log(item);
+  };
+
   render() {
     const { product, currency } = this.props;
     return (
-      <div className="Form">
+      <form className="Form" onSubmit={this.addToCart}>
         <h1>{product.brand}</h1>
         <h1>{product.name}</h1>
         <ul className="attributes">
@@ -15,29 +25,40 @@ class Form extends Component {
             <li key={attribute.name}>
               <h3>{attribute.name}:</h3>
               <div className="attribute">
-                {attribute.type !== "swatch"
-                  ? attribute.items.map((item) => (
-                      <span key={item.id} className="text">
+                {attribute.items.map((item) => (
+                  <span key={item.id} className="item">
+                    <input
+                      type="checkbox"
+                      onClick={() => this.handleChange(attribute, item)}
+                    />
+                    {attribute.type !== "swatch" ? (
+                      <label
+                        className="text"
+                        onClick={() => this.handleChange(attribute, item)}
+                      >
                         {item.displayValue}
-                      </span>
-                    ))
-                  : attribute.items.map((item) => (
-                      <span className="swatch" key={item.id}>
-                        <div
-                          style={{
-                            backgroundColor: item.value,
-                            color: "transparent",
-                          }}
-                        ></div>
-                      </span>
-                    ))}
+                      </label>
+                    ) : (
+                      <div
+                        className="swatch"
+                        style={{
+                          backgroundColor: item.value,
+                        }}
+                        onClick={() => this.handleChange(attribute, item)}
+                      >
+                        <div></div>
+                      </div>
+                    )}
+                  </span>
+                ))}
               </div>
             </li>
           ))}
         </ul>
         <h3>price:</h3>
-        <span>{getPrice(product.prices, currency)}</span>
-      </div>
+        <span id="price">{getPrice(product.prices, currency)}</span>
+        <button type="submit">add to cart</button>
+      </form>
     );
   }
 }
