8d5ee3baefdd88919590cb8d83ccf54a7fc2b1f4
Nemanja <dobranicnemanja2014@gmail.com>
2022-09-24 19:07:19 +0200

    completed checkout

--- src/modules/checkout/Checkout.js
+++ src/modules/checkout/Checkout.js
@@ -2,23 +2,35 @@ import React, { Component } from "react";
 import "./checkout.css";
 import { connect } from "react-redux";
 import Cart from "../../components/cart/Cart";
+import { getPrice } from "../../resources/commonFunctions/commonFunctions";
+import { Redirect } from "react-router-dom";
 
 class Checkout extends Component {
-  shouldComponentUpdate(nextProps) {
-    const { quantity, history } = nextProps;
-    if (!quantity) {
-      history.push("/");
-      return false;
-    }
+  state = {};
 
-    return true;
-  }
+  getTax = (bill, tax, currency) =>
+    currency.symbol + Math.round(tax * bill * 100) / 100;
 
   render() {
+    const { tax, totalPrices, currency, quantity } = this.props;
+
+    if (!quantity) return <Redirect exact to="/" />;
+
+    const bill = parseFloat(getPrice(totalPrices, currency).substring(1));
+
     return (
       <div className="Checkout">
         <h1>cart</h1>
         <Cart slidableGallery={true} />
+        <dl>
+          <dt>Tax {tax * 100}%:</dt>
+          <dd>{this.getTax(bill, tax, currency)}</dd>
+          <dt>Quantity:</dt>
+          <dd>{quantity}</dd>
+          <dt>Total:</dt>
+          <dd>{getPrice(totalPrices, currency)}</dd>
+        </dl>
+        <button>order</button>
       </div>
     );
   }
@@ -28,6 +40,8 @@ const mapStateToProps = (state) => {
   return {
     products: [...state.cart.products],
     quantity: state.cart.quantity,
+    tax: state.cart.tax,
+    totalPrices: state.cart.totalPrices,
     currency: state.navbar.currency,
   };
 };
